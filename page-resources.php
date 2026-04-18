3f6f3f1e23b88cfed24e9fedeac95bfaa9ea8cf2
Zane Creek <creekze@gmail.com>
2026-04-18 03:24:20 -0500

    Move about page styles from template into style.css

--- page-resources.php
+++ page-resources.php
@@ -8,7 +8,7 @@ get_header(); ?>
     <div class="container">
         <div class="archive-header__label">Learn</div>
         <h1 class="resources-header__title">Resources</h1>
-        <p class="resources-header__subtitle">The best tools, guides, and references for local SEO — curated and updated
+        <p class="resources-header__subtitle">The best guides, tool, and references for local SEO — curated and updated
             regularly.</p>
     </div>
 </div>
