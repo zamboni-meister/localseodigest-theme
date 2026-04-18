485ae558bcbf45683b20c2e3621fe6f3a30aa47a
Zane Creek <creekze@gmail.com>
2026-04-18 01:21:32 -0500

    Open newsletter confirmation in new tab

--- page-newsletter.php
+++ page-newsletter.php
@@ -36,7 +36,7 @@ get_header();
 
 		<!-- Form -->
 		<div class="lsd-nl-page__form-wrap">
-			<form class="lsd-nl-page__form" method="POST"
+			<form class="lsd-nl-page__form" method="POST" target="_blank"
 				action="https://1c0cb842.sibforms.com/serve/MUIFACfB43trusSs2v3WjJyc3X8hMfxr031W92rOqPBY-RgYtAkikvYykYSkl2vxmAMxGF_uyJGnvJTbl3e3zThuAopZeArAR8Mv-agnDrZhKKfe7ZMKGO_OXG0V16MfdJaa6TR4tMtg9ClJrqtITc9dI87d55Doe2BEi1OBTDcmw0MezVbF0BTvojWOvu79yhSWyVrtYgy5eghj">
 				<label for="lsd-nl-email" class="lsd-nl-page__form-label">Your email address</label>
 				<div class="lsd-nl-page__form-row">
@@ -53,6 +53,7 @@ get_header();
 				</div>
 				<input type="text" name="email_address_check" value="" style="display:none;">
 				<input type="hidden" name="locale" value="en">
+                <input type="hidden" name="redirectionUrl" value="https://sibforms.com/confirmation/success/subscription/double?locale=en">
 			</form>
 			<p class="lsd-nl-page__disclaimer">We'll never sell or misuse your info. Unsubscribe any time.</p>
 		</div>
